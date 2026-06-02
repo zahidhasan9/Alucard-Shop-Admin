a327366ad643131f3529cb8f07392d5afff04d10
Jahid Hasan Rimel <zmzahidhasan181@gmail.com>
2026-06-03 03:46:16 +0600

    fix route in server

--- src/app/ecommerce/layout.js
+++ src/app/ecommerce/layout.js
@@ -1,13 +1,25 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+// import { cookies } from 'next/headers';
+// import { redirect } from 'next/navigation';
 
-export default async function DashboardLayout({ children }) {
-  const cookieStore = await cookies();
-  const token = cookieStore.get('token');
+// export default async function DashboardLayout({ children }) {
+//   const cookieStore = await cookies();
+//   const token = cookieStore.get('token');
 
-  if (!token) {
-    redirect('/authentication/sign-in/');
-  }
+//   if (!token) {
+//     redirect('/authentication/sign-in/');
+//   }
 
+//   return <>{children}</>;
+// }
+
+
+
+'use client';
+
+import PrivateRoute from '@/HOC/PrivateRoute';
+
+function EcommerceLayout({ children }) {
   return <>{children}</>;
 }
+
+export default PrivateRoute(EcommerceLayout);
