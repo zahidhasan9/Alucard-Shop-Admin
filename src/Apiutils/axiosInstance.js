f6cb51f57d2538761ef1d53726d3f498159a2e6d
Jahid Hasan Rimel <zmzahidhasan181@gmail.com>
2026-06-03 22:34:40 +0600

    update

--- src/Apiutils/axiosInstance.js
+++ src/Apiutils/axiosInstance.js
@@ -13,7 +13,8 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+  // process.env.NEXT_PUBLIC_API_URL ||
+   'http://localhost:5000/api';
 
 if (
   typeof window !== 'undefined' &&
--- src/components/Layout/LeftSidebar.js
+++ src/components/Layout/LeftSidebar.js
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import { Accordion } from 'react-bootstrap';
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
-import Image from 'next/image';
+import { Accordion } from "react-bootstrap";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
 
 const LeftSidebar = ({ toogleActive }) => {
   const pathname = usePathname();
@@ -42,7 +42,7 @@ const LeftSidebar = ({ toogleActive }) => {
               <Accordion.Header>
                 <i className="material-symbols-outlined">dashboard</i>
                 <span className="title">Dashboard</span>
-                <span className="count">18</span>
+                {/* <span className="count">18</span> */}
               </Accordion.Header>
               <Accordion.Body>
                 <ul className="sub-menu">
@@ -75,12 +75,13 @@ const LeftSidebar = ({ toogleActive }) => {
                 <ul className="sub-menu">
                   <li className="menu-item">
                     <Link
-                      href="/ecommerce/products-grid/"
-                      className={`menu-link ${pathname === "/ecommerce/products-grid/" ? "active" : ""}`}
+                      href="/ecommerce/create-product/"
+                      className={`menu-link ${pathname === "/ecommerce/create-product/" ? "active" : ""}`}
                     >
-                      Products Grid
+                      Create Product
                     </Link>
                   </li>
+
                   <li className="menu-item">
                     <Link
                       href="/ecommerce/products-list/"
@@ -97,22 +98,6 @@ const LeftSidebar = ({ toogleActive }) => {
                       Product Moderation
                     </Link>
                   </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/product-details/"
-                      className={`menu-link ${pathname === "/ecommerce/product-details/" ? "active" : ""}`}
-                    >
-                      Product Details
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/create-product/"
-                      className={`menu-link ${pathname === "/ecommerce/create-product/" ? "active" : ""}`}
-                    >
-                      Create Product
-                    </Link>
-                  </li>
 
                   <li className="menu-item">
                     <Link
@@ -125,28 +110,13 @@ const LeftSidebar = ({ toogleActive }) => {
 
                   <li className="menu-item">
                     <Link
-                      href="/ecommerce/cart/"
-                      className={`menu-link ${pathname === "/ecommerce/cart/" ? "active" : ""}`}
-                    >
-                      Cart
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
                       href="/ecommerce/category-brand/"
                       className={`menu-link ${pathname === "/ecommerce/category-brand/" ? "active" : ""}`}
                     >
                       Category Brand
                     </Link>
                   </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/checkout/"
-                      className={`menu-link ${pathname === "/ecommerce/checkout/" ? "active" : ""}`}
-                    >
-                      Checkout
-                    </Link>
-                  </li>
+
                   <li className="menu-item">
                     <Link
                       href="/ecommerce/orders/"
@@ -155,147 +125,13 @@ const LeftSidebar = ({ toogleActive }) => {
                       Orders
                     </Link>
                   </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/orders/details/"
-                      className={`menu-link ${pathname === "/ecommerce/orders/details/" ? "active" : ""}`}
-                    >
-                      Orders Details
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/orders/create/"
-                      className={`menu-link ${pathname === "/ecommerce/orders/create/" ? "active" : ""}`}
-                    >
-                      Create Order
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/orders/tracking/"
-                      className={`menu-link ${pathname === "/ecommerce/orders/tracking/" ? "active" : ""}`}
-                    >
-                      Order Tracking
-                    </Link>
-                  </li>
+
                   <li className="menu-item">
                     <Link
                       href="/ecommerce/customers/"
                       className={`menu-link ${pathname === "/ecommerce/customers/" ? "active" : ""}`}
                     >
                       Customers
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/customers/details/"
-                      className={`menu-link ${pathname === "/ecommerce/customers/details/" ? "active" : ""}`}
-                    >
-                      Customer Details
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/categories/"
-                      className={`menu-link ${pathname === "/ecommerce/categories/" ? "active" : ""}`}
-                    >
-                      Categories
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/sellers/"
-                      className={`menu-link ${pathname === "/ecommerce/sellers/" ? "active" : ""}`}
-                    >
-                      Sellers
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/sellers/details/"
-                      className={`menu-link ${pathname === "/ecommerce/sellers/details/" ? "active" : ""}`}
-                    >
-                      Sellers Details
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/sellers/create/"
-                      className={`menu-link ${pathname === "/ecommerce/sellers/create/" ? "active" : ""}`}
-                    >
-                      Create Seller
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/reviews/"
-                      className={`menu-link ${pathname === "/ecommerce/reviews/" ? "active" : ""}`}
-                    >
-                      Reviews
-                    </Link>
-                  </li>
-                  <li className="menu-item">
-                    <Link
-                      href="/ecommerce/refunds/"
-                      className={`menu-link ${pathname === "/ecommerce/refunds/" ? "active" : ""}`}
-                    >
-                      Refunds
-                    </Link>
-                  </li>
-                </ul>
-              </Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey="12">
-              <Accordion.Header>
-                <i className="material-symbols-outlined">local_activity</i>
-                <span className="title">Events</span>
-              </Accordion.Header>
-              <Accordion.Body>
-                <ul className="sub-menu">
-                  <li className="menu-item">
-                    <Link
-                      href="/events/"
-                      className={`menu-link ${pathname === "/events/" ? "active" : ""}`}
-                    >
-                      Events Grid
-                    </Link>
-                  </li>
-
-                  <li className="menu-item">
-                    <Link
-                      href="/events/list/"
-                      className={`menu-link ${pathname === "/events/list/" ? "active" : ""}`}
-                    >
-                      Events List
-                    </Link>
-                  </li>
-
-                  <li className="menu-item">
-                    <Link
-                      href="/events/details/"
-                      className={`menu-link ${pathname === "/events/details/" ? "active" : ""}`}
-                    >
-                      Event Details
-                    </Link>
-                  </li>
-
-                  <li className="menu-item">
-                    <Link
-                      href="/events/create-an-event/"
-                      className={`menu-link ${pathname === "/events/create-an-event/" ? "active" : ""}`}
-                    >
-                      Create An Event
-                    </Link>
-                  </li>
-
-                  <li className="menu-item">
-                    <Link
-                      href="/events/edit-an-event/"
-                      className={`menu-link ${pathname === "/events/edit-an-event/" ? "active" : ""}`}
-                    >
-                      Edit An Event
                     </Link>
                   </li>
                 </ul>
